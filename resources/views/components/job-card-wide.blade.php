db9e1558bbf98188673d91826cdc241fb49f4c34
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-01-10 23:03:57 +0300

    update

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,15 @@
+<div class="p-3 bg-white/5 rounded-xl flex gap-x-6">
+    <div>
+        <img src="http://picsum.photos/seed/{{ rand(1,100) }}/100/100" alt="Placeholder" class="rounded-xl">
+    </div>
+    <div class="py-8 font-bold flex-1">
+        <a href="#" class="self-start text-sm">GoyExec</a>
+        <h3>Full Stack Laravel Developer</h3>
+        <p>Fulltime - from $60000</p>
+    </div>
+    <div class="flex items-center justify-between mt-auto gap-x-5">
+        <x-tag>FrontEnd</x-tag>
+        <x-tag>Backend</x-tag>
+        <x-tag>Api</x-tag>
+    </div>
+</div>
