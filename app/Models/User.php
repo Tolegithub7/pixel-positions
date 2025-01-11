ef556398cefa574161433c5a4659d94c1a43331a
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-01-11 19:18:51 +0300

    create jobs and assign relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function Employer()
+    {
+        return $this->hasOne(Employer::class);
+    }
 }
