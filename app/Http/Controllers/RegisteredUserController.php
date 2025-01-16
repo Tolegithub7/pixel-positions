df53d2eb34327362f243ae380a0382527ac36a75
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-01-16 22:16:14 +0300

    update

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -1,1 +1,103 @@
-html5.html
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
+
+// use Illuminate\Support\Facades\Password;
+// use Illuminate\Http\File;
+// use Illuminate\Support\Facades\Auth;
+// use Illuminate\Validation\Rules\Password;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('auth.register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validate user attributes
+        $userAttributes = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', Password::min(6)],
+        ]);
+
+        // Validate employer attributes
+        $employerAttributes = $request->validate([
+            'employer' => ['required'],
+            'logo' => ['required', File::types(['png', 'jpg', 'webp'])],
+            // 'logo' => ['required', 'mimes:png,jpg,webp'],
+        ]);
+
+        // Create the user
+        $user = User::create($userAttributes);
+
+        // Store the employer logo $employerAttributes['logo'] = 
+        $logoPath = $request->logo->store('logos');
+
+        // Create the employer associated with the user
+        $user->employer()->create([
+            'name' => $employerAttributes['employer'],
+            'logo' => $logoPath,
+        ]);
+
+        // Log in the newly created user
+        Auth::login($user);
+        return redirect('/');
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
