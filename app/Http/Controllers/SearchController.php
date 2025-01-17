a405aa9e807d36b9076fa8c3dbef25a970d0782c
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-01-18 02:58:55 +0300

    update 30:30's serach

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class SearchController extends Controller
 {
     //
+    public function __invoke()
+    {
+        $jobs = Job::where('title', 'LIKE', '%' . request('q') . '%')->get();
+        return $jobs;
+    }
 }
